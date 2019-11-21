1e0c4572a7f117d5d55209cafa70b80e10897de8
IvanS <ivan443931@gmail.com>
2019-11-21 23:18:30 +0300

    Move Logger factories to Domain

--- src/SO.DataAccess/DbContext/SolutionOneDbContextOptionsConfiguration.cs
+++ src/SO.DataAccess/DbContext/SolutionOneDbContextOptionsConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using SO.Domain.Logger;
 
 namespace SO.DataAccess.DbContext
 {
---
+++ src/SO.Domain/Logger/LoggerFactories.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace SO.Domain.Logger
+{
+    public static class LoggerFactories
+    {
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder => { builder.AddConsole(); });
+    }
+}
