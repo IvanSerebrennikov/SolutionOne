452604633ffbf19c7a6a1c68bc4b552b54b36fc1
IvanS <ivan443931@gmail.com>
2019-11-23 22:03:35 +0300

    Add simple RabbitMQ producer implementation, add Newtonsoft.Json package

--- src/SO.Infrastructure/MessageProducing/RabbitMQMessageProducer.cs
+++ src/SO.Infrastructure/MessageProducing/RabbitMQMessageProducer.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using AMPQMessages.Messages;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 using SO.Domain.InfrastructureInterfaces.MessageProducing;
 
 namespace SO.Infrastructure.MessageProducing
@@ -10,7 +12,26 @@ namespace SO.Infrastructure.MessageProducing
     {
         public void ProduceCityCreated(CityCreatedMessage message)
         {
-            // ...
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "CityCreatedTest",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "CityCreatedTest",
+                basicProperties: null,
+                body: body);
         }
     }
 }
